2fec4d6188b9acbf58b7dd191ad7c96f63ff3ed8
LHaringSARAO <lharing@sarao.ac.za>
2025-01-20 09:40:56 +0200

    Add code to connect fastapi query to a launch of turtleg

--- backend.py
+++ backend.py
@@ -2,12 +2,16 @@ import turtle
 from time import sleep
 
 # Screen stuff
-wn = turtle.Screen()
-wn.bgcolor()
-wn.title("Title")
 
 
-skk = turtle.Turtle()
-skk.forward(100)
-turtle.done()
-sleep(2)
+def func_turtle():
+
+    wn = turtle.Screen()
+    wn.bgcolor()
+    wn.title("Title")
+
+
+    skk = turtle.Turtle()
+    skk.forward(100)
+    turtle.done()
+    sleep(2)
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 
+
+from backend import func_turtle
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"Hello" : "Root"}
+
+
+@app.get("/test_turtle")
+def test_turtle():
+    func_turtle()
+    return None
