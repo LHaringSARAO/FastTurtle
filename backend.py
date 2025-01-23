9ce1250c12fef38a04109d81ab9269537fb136d6
LHaringSARAO <lharing@sarao.ac.za>
2025-01-23 18:57:25 +0200

    Comment out current unused method

--- backend.py
+++ backend.py
@@ -1,17 +1,17 @@
-import turtle
-from time import sleep
+# import turtle
+# from time import sleep
 
-# Screen stuff
-
-
-def func_turtle():
-
-    wn = turtle.Screen()
-    wn.bgcolor()
-    wn.title("Title")
+# # Screen stuff
 
 
-    skk = turtle.Turtle()
-    skk.forward(100)
-    turtle.done()
-    sleep(2)
+# def func_turtle():
+
+#     wn = turtle.Screen()
+#     wn.bgcolor()
+#     wn.title("Title")
+
+
+#     skk = turtle.Turtle()
+#     skk.forward(100)
+#     turtle.done()
+#     sleep(2)
